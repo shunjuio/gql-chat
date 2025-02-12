3a677ee1833eb1a4f0666d89ab94b7f7f1728bb9
saboyutaka <yutaka.paradigm.shift@gmail.com>
2025-02-13 00:42:45 +0900

    application.rbを整理する

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,22 @@
 require_relative "boot"
 
-require "rails/all"
+# require "rails/all"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+# require "active_storage/engine"
+# require "action_mailer/railtie"
+# require "active_job/railtie"
+require "action_cable/engine"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module App
+module GqlChat
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
