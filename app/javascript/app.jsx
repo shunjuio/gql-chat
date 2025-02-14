749c872677df68f89efa2f8eee8428d1a149f8f5
ext-shimoji <k.shimoji@shunju.io>
2025-02-14 18:12:34 +0900

    フロント側のmutation実装

--- app/javascript/app.jsx
+++ app/javascript/app.jsx
@@ -1,6 +1,6 @@
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import { graphql } from "./gql";
-import React from "react";
+import React, { useState } from "react";
 
 const GET_MESSAGES = graphql(`
   query GetMessages {
@@ -13,21 +13,57 @@ const GET_MESSAGES = graphql(`
   }
 `);
 
-const Form = function () {
+const CREATE_MESSAGE = graphql(`
+  mutation CreateMessage($senderName: String!, $content: String!) {
+    createMessage(input: { senderName: $senderName, content: $content }) {
+      message {
+        id
+        senderName
+        content
+        createdAt
+      }
+    }
+  }
+`);
+
+const Form = function ({ createMessage }) {
+  const [senderName, setSenderName] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createMessage({
+      variables: { senderName, content },
+    }).then(() => {
+      setSenderName("");
+      setContent("");
+    });
+  };
+
   return (
     <div className="pt-2">
-      <form className="flex flex-col border-1 px-4 py-2">
+      <form
+        className="flex flex-col border-1 px-4 py-2"
+        onSubmit={handleSubmit}
+      >
         <input
+          value={senderName}
           type="text"
           placeholder="name"
           className="max-w-32 border border-gray-300 rounded-md p-2"
+          onChange={(e) => setSenderName(e.target.value)}
         />
         <textarea
+          value={content}
           placeholder="content"
           className="mx-10 border border-gray-300 rounded-md p-2 my-2"
+          onChange={(e) => setContent(e.target.value)}
         />
-        <button className="mx-auto max-w-20 bg-blue-500 text-white rounded-md p-2 hover:bg-blue-600">
-          submit
+        <button
+          className="mx-auto max-w-20 bg-blue-500 text-white rounded-md p-2 hover:bg-blue-600"
+          type="submit"
+        >
+          投稿する
         </button>
       </form>
     </div>
@@ -55,10 +91,35 @@ const List = function ({ messages }) {
 
 const App = function () {
   const { data, loading, error } = useQuery(GET_MESSAGES);
+  const [createMessage] = useMutation(CREATE_MESSAGE, {
+    update(cache, { data }) {
+      const message = data.createMessage.message;
+      cache.modify({
+        fields: {
+          messages(existingMessages = []) {
+            const newMessageRef = cache.writeFragment({
+              data: message,
+              fragment: graphql(
+                `
+                  fragment NewMessage on Message {
+                    id
+                    senderName
+                    content
+                    createdAt
+                  }
+                `
+              ),
+            });
+            return [newMessageRef, ...existingMessages];
+          },
+        },
+      });
+    },
+  });
 
   return (
     <div className="mx-auto max-w-[640px] px-10">
-      <Form />
+      <Form createMessage={createMessage} />
       <List messages={data?.messages ?? []} />
     </div>
   );
