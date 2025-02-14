0f79f2a0daf6595233359cf1b38c0056c9817af0
saboyutaka <yutaka.paradigm.shift@gmail.com>
2025-02-14 10:27:08 +0900

    Message Typeのfieldを修正
    - id をglobal_idの値で出力
    - updatedAt は不要

--- app/graphql/types/message_type.rb
+++ app/graphql/types/message_type.rb
@@ -2,10 +2,10 @@
 
 module Types
   class MessageType < Types::BaseObject
-    field :id, ID, null: false
+    global_id_field :id
+
     field :sender_name, String, null: false
     field :content, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
