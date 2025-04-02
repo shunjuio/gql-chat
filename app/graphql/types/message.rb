76389cfea7663d5a157c5814ed0741c29e7e2c8c
saboyutaka <yutaka.paradigm.shift@gmail.com>
2025-04-02 15:31:26 +0900

    IDをglobal_idにする 値を "Message/1" のBase64 にする

--- app/graphql/types/message.rb
+++ app/graphql/types/message.rb
@@ -2,7 +2,7 @@ module Types
   class Message < Types::BaseObject
     implements Interfaces::Node
 
-    field :id, ID, null: false
+    global_id_field :id
     field :sender_name, String, null: false
     field :content, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
