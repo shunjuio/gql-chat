7376eb9e16b59335e546d09b22e7b56ea86ab654
saboyutaka <yutaka.paradigm.shift@gmail.com>
2025-06-11 15:09:42 +0900

    参考実装を元にリファクタリング
    https://graphql-ruby.org/javascript_client/apollo_subscriptions.html#apollo-link--actioncable

--- app/javascript/client.js
+++ app/javascript/client.js
@@ -1,65 +1,6 @@
-import { ApolloClient, ApolloLink, InMemoryCache, split, HttpLink, Observable } from '@apollo/client';
-import { getMainDefinition } from '@apollo/client/utilities';
-import consumer from './channels/index';
-import { print } from 'graphql/language/printer';
-
-// ActionCable WebSocketリンクを作成するクラス
-class ActionCableLink extends ApolloLink {
-  constructor(options) {
-    super(); // ApolloLink を継承する場合は super() を呼ぶ
-    this.consumer = options.consumer;
-  }
-
-  request(operation) {
-    return new Observable(observer => {
-      const channel = this.consumer.subscriptions.create(
-        { channel: 'GraphqlChannel' }, // サーバー側のチャンネル名と一致させる
-        {
-          connected: () => {
-            try {
-              const queryAsString = print(operation.query); // print関数を使ってクエリ文字列を取得
-              channel.perform('execute', {
-                query: queryAsString,
-                variables: operation.variables,
-                operationName: operation.operationName,
-              });
-            } catch (error) {
-              console.error("ActionCableLink: Error performing execute:", error);
-              observer.error(error);
-            }
-          },
-          received: (data) => {
-            if (data.result) {
-              if (data.result.data) {
-                observer.next(data.result);
-              }
-              if (data.result.errors) {
-                console.error("ActionCableLink: GraphQL errors received:", data.result.errors);
-                observer.error(new Error(data.result.errors.map(e => e.message).join('\n')));
-              }
-            } else if (data.errors) {
-              console.error("ActionCableLink: Top-level errors received:", data.errors);
-              observer.error(new Error(data.errors.map(e => e.message).join('\n')));
-            } else if (data.type === "reject_subscription") {
-              console.error("ActionCableLink: Subscription rejected by server");
-              observer.error(new Error("Subscription rejected"));
-            }
-          },
-          disconnected: () => {
-            console.log("ActionCableLink: WebSocket disconnected");
-            observer.error(new Error("WebSocket disconnected")); // 再接続のためにエラーを投げる
-          }
-        }
-      );
-
-      // unsubscribe時の処理
-      return () => {
-        console.log("ActionCableLink: Unsubscribing and disconnecting channel");
-        channel.unsubscribe();
-      };
-    });
-  }
-}
+import { ApolloClient, InMemoryCache, ApolloLink, HttpLink } from '@apollo/client';
+import ActionCableLink from 'graphql-ruby-client/subscriptions/ActionCableLink';
+import { createConsumer } from '@rails/actioncable'
 
 // HTTP通信用のリンク
 const httpLink = new HttpLink({
@@ -71,18 +12,17 @@ const httpLink = new HttpLink({
 
 // WebSocket用のリンク
 const cableLink = new ActionCableLink({
-  consumer
+  cable: createConsumer('/cable')
 });
 
-// リンクを分割する
-const link = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    );
-  },
+const hasSubscriptionOperation = ({ query: { definitions } }) => {
+  return definitions.some(
+    ({ kind, operation }) => kind === 'OperationDefinition' && operation === 'subscription'
+  )
+}
+
+const link = ApolloLink.split(
+  hasSubscriptionOperation,
   cableLink,
   httpLink
 );
